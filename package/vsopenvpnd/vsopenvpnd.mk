77e348035e32849631d2409baa53e933f4a367b4
Vision Systems GmbH <contact@visionsystems.de>
2017-03-07 12:11:33 +0100

    vsopenvpnd: install stubs

--- package/vsopenvpnd/vsopenvpnd.mk
+++ package/vsopenvpnd/vsopenvpnd.mk
@@ -10,6 +10,7 @@ VSOPENVPND_SITE = ftp://ftp.visionsystems.de/pub/multiio/OnRISC/Baltos/viaVPN
 define VSOPENVPND_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/zlog.conf $(TARGET_DIR)/etc/zlog.conf
 	$(INSTALL) -D -m 0755 $(@D)/vsopenvpnd $(TARGET_DIR)/usr/bin/vsopenvpnd
+	cp -r $(@D)/viavpn $(TARGET_DIR)/opt
 endef
 
 $(eval $(generic-package))
