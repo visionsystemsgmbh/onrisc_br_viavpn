f8be70bcd3195eb30663856f446b7b0b493f484f
Vision Systems GmbH <contact@visionsystems.de>
2020-12-22 10:35:37 +0100

    vsopenvpnd: bump to version 1.0.5
    Remove zlog support and external toolchain dependency.

--- package/vsopenvpnd/vsopenvpnd.mk
+++ package/vsopenvpnd/vsopenvpnd.mk
@@ -3,12 +3,11 @@
 # vsopenvpnd
 #
 #############################################################
-VSOPENVPND_VERSION = 1.0.0
+VSOPENVPND_VERSION = 1.0.5
 VSOPENVPND_SOURCE = vsopenvpnd-$(VSOPENVPND_VERSION).tar.xz
 VSOPENVPND_SITE = ftp://ftp.visionsystems.de/pub/multiio/OnRISC/Baltos/viaVPN
 
 define VSOPENVPND_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/zlog.conf $(TARGET_DIR)/etc/zlog.conf
 	$(INSTALL) -D -m 0755 $(@D)/vsopenvpnd $(TARGET_DIR)/usr/bin/vsopenvpnd
 	cp -r $(@D)/viavpn $(TARGET_DIR)/opt
 endef
